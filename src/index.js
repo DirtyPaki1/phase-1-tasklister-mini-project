8328762b8aa2ae32e5d3c860fc706eaa9ba46028
Usman Qidwai <usmanqidwai21@gmail.com>
2023-10-03 15:11:57 -0400

    Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+  form.addEventListener('submit', function(event) {
+  
+    event.preventDefault();
+  
+    
+    const taskInput = document.getElementById('new-task-description');
+    const task = taskInput.value;
+  
+   
+    const taskElement = document.createElement('li');
+    taskElement.innerText = task;
+  
+   
+    const taskList = document.getElementById('new-task-description');
+    taskList.appendChild(taskElement);
+  
+    
+    taskInput.value = '';
+  });
+
+  
 });
